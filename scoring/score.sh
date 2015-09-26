27a9b617fd90b345b0f588d162cc02808d0da4ec
csc <csc@csc-desktop.(none)>
2015-09-26 12:37:19 -0800

    Added crude scoring engine script. Doesn't keep score, just dumps useful stuff to screen.

---
+++ scoring/score.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+port=1234
+
+(
+while [ true ]
+do
+	# Make list of IP addresses with service open
+	sudo nmap -sS --open -p$port 10.250.100.100-254 > map || exit 1
+	grep Interesting map | sed -e 's/://' | awk '{print $4}' > IP || exit 1
+
+	# Show output of each service
+	echo
+	date
+	echo "Scan found "`wc -l IP`" hosts:"
+	for host in `cat IP`
+	do
+		name=`grep "$host " names`
+		if [ "$name" ]
+		then
+			echo -n "$name: "
+		else
+			echo -n "$host: "
+		fi
+		echo "Dr. Lawlor" | nc -w 1 $host $port
+	done
+	sleep 15 || exit 1
+done
+) | tee log
